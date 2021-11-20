da6399273f27d686400c2738326fa62f7d707bcb
rldunkle <rldunkle@ncsu.edu>
2021-11-20 14:28:49 -0500

    Import packages

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import math as ma
 import numpy as np
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
-from matplotlib import (rcParams, style)
-from matplotlib import backend_bases
+from matplotlib import (rcParams, style, backend_bases)
 
 # Prevents axes from being cut off in graph
 rcParams.update({'figure.autolayout': True})
@@ -12,9 +11,8 @@ rcParams.update({'figure.autolayout': True})
 # Acceptable characters for list entries; used in regression function to check for list entry errors
 acceptedCharacters = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '.'}
 
-# Converts a string of digits into superscript with unicode for exponents and list index labels
+# Converts a string of digits into superscript for exponents and list index labels
 def superscript(exponent):
     sup = str.maketrans("0123456789.", "⁰¹²³⁴⁵⁶⁷⁸⁹˙")
     exp = exponent.translate(sup)
     return exp
-
